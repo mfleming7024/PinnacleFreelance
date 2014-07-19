6ca088ab76f1dc6fee6f54ee3242f19a3755a613
Michael Fleming <mfleming7024@gmail.com>
2014-07-19 01:28:37 -0400

    fully responsive drop downs

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,12 @@
-//Grid pixel/class name animation
-$("#grid_system .row div").hover(function() {
-    var that = $(this);
-    that.find("h2").stop().animate({opacity: 0, left: "20px"}, 400);
-    that.find("h3").stop().animate({opacity: 1, right: "0px"}, 400);
-    
-}, function(){
-    var that = $(this);
-    that.find("h2").stop().animate({opacity: 1, left: "0px"}, 400);
-    that.find("h3").stop().animate({opacity: 0, right: "20px"}, 400);
-});
-
-//Colors id name animation
-$("#colors .row div div").hover(function() {
-    var that = $(this);
-    that.find("h2").stop().animate({opacity: 1}, 400);
-    
-}, function(){
-    var that = $(this);
-    that.find("h2").stop().animate({opacity: 0}, 300);
-});
-
 //Navigation bar triggering?
 $("nav.responsive .hidden a#menu").on("click", function(e){
     e.preventDefault();
     
     var that = $(this);
     
-    var menu = that.parent().parent().find("ul");
+//    var menu = that.parent().parent().find("ul");
+	var menu = $("nav.responsive > ul");
+	
     
     if (menu.css('display') == 'none'){
         menu.css("display", "block");
